eb8b8123cbbfc107d1b89a7d7cb2cc8f931f2768
Alx19 <alx19sucharev@gmail.com>
2023-01-29 20:30:35 -0600

    stop overwritting logs

--- my_logger.rb
+++ my_logger.rb
@@ -1,5 +1,5 @@
 class MyLogger
   def log(message)
-    File.open('log.txt', 'w') { |f| f.write "#{Time.now}: #{message}" }
+    File.open('log.txt', 'a') { |f| f.write "#{Time.now}: #{message}" }
   end
 end
