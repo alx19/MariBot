fe9f47da89fa55d5c61346aafe94a4c16e325573
Aleksandr Sukharev <alx19sucharev@gmail.com>
2025-02-07 14:20:01 +0300

    ooops had to revoke token

--- alert.rb
+++ alert.rb
@@ -1,5 +1,5 @@
 require 'telegram/bot'
 
 if `pgrep --f "ruby main.rb"`.split("\n").count == 1
-  Telegram::Bot::Client.new('6024269718:AAHcTb7c-2XmHk9jiwoUDaiK7VZsCPzO_fA').api.send_message(chat_id: 173948014, text: 'бот Мари упал!')
+  Telegram::Bot::Client.new('spoonbot_token').api.send_message(chat_id: 173948014, text: 'бот Мари упал!')
 end
